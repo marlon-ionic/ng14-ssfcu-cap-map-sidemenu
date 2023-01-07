9d9d53619ef79f9b4213f73b015bc07f400afad4
Marlon Harrison <marlon@ionic.io>
2023-01-06 18:34:01 -0600

    update cap webDir config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'issue-cap-map-sidemenu',
-  webDir: 'www',
+  webDir: 'dist/ng14-ssfcu-cap-map-sidemenu',
   bundledWebRuntime: false
 };
 
